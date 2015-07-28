f519659f597a068d776a60a54620f1085238eb87
Hyogi Sim <hyogi@vt.edu>
2015-07-28 17:11:06 -0400

    imess skeleton added

---
+++ xlators/features/imess/src/imess.h
@@ -0,0 +1,18 @@
+/* Copyright (C) 2015	 - Hyogi Sim <simh@ornl.gov>
+ * 
+ * Please refer to COPYING for the license.
+ * ---------------------------------------------------------------------------
+ * 
+ */
+#ifndef _IMESS_H_
+#define	_IMESS_H_
+
+#ifndef _CONFIG_H
+#define _CONFIG_H
+#include "config.h"
+#endif
+
+#include "xlator.h"
+
+#endif	/* _IMESS_H_ */
+
