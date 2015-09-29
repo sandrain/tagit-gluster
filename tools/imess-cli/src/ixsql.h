94091464546a2a1e48f3ad7fd7c8cf1cc277f29e
Hyogi Sim <hyogi@vt.edu>
2015-09-29 12:29:24 -0400

    a bit reorganization of ixsql

--- tools/imess-cli/src/ixsql.h
+++ tools/imess-cli/src/ixsql.h
@@ -10,7 +10,38 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <sys/time.h>
-#include "ix-imess.h"
+
+#include "glfs.h"
+#include "glfs-handles.h"
+
+/* gluster internal headers */
+#undef _CONFIG_H
+#include "glusterfs.h"
+#include "xlator.h"
+
+#include <sys/time.h>
+
+static inline void
+timeval_latency (struct timeval *out,
+		 struct timeval *before, struct timeval *after)
+{
+	time_t sec       = 0;
+	suseconds_t usec = 0;
+
+	if (!out || !before || !after)
+		return;
+
+	sec = after->tv_sec - before->tv_sec;
+	if (after->tv_usec < before->tv_usec) {
+		sec -= 1;
+		usec = 1000000 + after->tv_usec - before->tv_usec;
+	}
+	else
+		usec = after->tv_usec - before->tv_usec;
+
+	out->tv_sec = sec;
+	out->tv_usec = usec;
+}
 
 #define IXSQL_QUIT	0x12345
 
