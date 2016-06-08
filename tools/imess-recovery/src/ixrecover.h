fc2384b9821d14d0dc3338f53464ccd7dc9ba193
Hyogi Sim <hyogi@vt.edu>
2016-06-08 17:24:34 -0400

    wip of ixrecover

---
+++ tools/imess-recovery/src/ixrecover.h
@@ -0,0 +1,10 @@
+/* Copyright (C) 2016 	 - Hyogi Sim <simh@ornl.gov>
+ * 
+ * Please refer to COPYING for the license.
+ * ---------------------------------------------------------------------------
+ * 
+ */
+#ifndef _IXRECOVER_H_
+#define _IXRECOVER_H_
+
+#endif /* _IXRECOVER_H_ */
